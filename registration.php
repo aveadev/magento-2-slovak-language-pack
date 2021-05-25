a256437e0df5de4b20699acecfac91c0a54f6ea2
Matus <kobe21@gmail.com>
2021-05-25 08:29:06 +0200

    change to avea

--- registration.php
+++ registration.php
@@ -16,6 +16,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'svartsvan_sk_sk',
+    'aveadev_sk_sk',
     __DIR__
 );
